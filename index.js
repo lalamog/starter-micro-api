ccdaed3b002540bb43c0ba6314f3bda1b8be1743
lalamog <132582290+lalamog@users.noreply.github.com>
2023-05-04 22:33:07 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const express = require("express");
+const db = require("./routes/db-config");
+const app = express();
+const cookie = require("cookie-parser");
+const PORT = process.env.PORT || 3005;
+
+app.use("/js", express.static(__dirname + "/public/js"))
+app.use("/css", express.static(__dirname + "/public/css"))
+app.set("view engine" , "ejs");
+app.set("views" , "./views");
+app.use(cookie());
+app.use(express.json());
+db.connect((err)=>{
+    if(err)throw err;
+    console.log("database connected");
+})
+
+app.use("/", require("./routes/pages"));
+app.use("/api", require("./controllers/auth")); 
+app.listen(PORT);
